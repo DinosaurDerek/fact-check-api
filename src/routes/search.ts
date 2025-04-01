05fad74afc72645a64155cb655a52f39f4f7fed4
DinosaurDerek <derekjpaul@gmail.com>
2025-03-31 19:16:43 -0700

    adds api key authentication

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { Type } from '@sinclair/typebox'
 
+import { authenticate } from '../middlewares/authMiddleware.js'
+
 export default async function searchRoutes(fastify: FastifyInstance) {
   const SearchSchema = {
     response: {
@@ -11,8 +13,8 @@ export default async function searchRoutes(fastify: FastifyInstance) {
   }
 
   fastify.get(
-    '/',
-    { schema: SearchSchema },
+    '/search',
+    { preHandler: authenticate, schema: SearchSchema },
     async (_request: FastifyRequest, _reply: FastifyReply) => {
       return { message: 'Fastify API is running!' }
     }
